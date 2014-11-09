9f6b84545e6efbc3c23c18192a9f75ced2b7aadc
Greg Lubin <greg@greglubin.com>
2014-11-09 15:03:01 -0600

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+num = 1
+
+while num < 101:
+	if num % 3 == 0 and num % 5 == 0:
+		print "fizzbuzz"
+		num += 1
+	elif num % 3 == 0:
+		print "fizz"
+		num += 1
+	elif num % 5 == 0:
+		print "buzz"
+		num += 1
+	else:
+		print num
+		num +=1
+
